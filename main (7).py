8a0f6eaf463c95a822ddb906e649231beea31271
AkashG73 <127973727+AkashG73@users.noreply.github.com>
2024-01-31 23:05:30 +0530

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,10 @@
+a  = 1
+b  = 2.3
+c  = "Akash"
+d1 = True  # "T" should always be Capital
+d2 = False # "F" should always be capital
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d1))
+print(type(d2))
